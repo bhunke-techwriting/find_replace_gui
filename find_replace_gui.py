4aa4af18db2184f5540f83e39b8507d4da17e55e
bhunke-techwriting <135065729+bhunke-techwriting@users.noreply.github.com>
2023-06-05 12:29:42 -0400

    Update find_replace_gui.py
    Added support for bold and italics. Added conditions to prevent duplicate paragraph tags.

--- find_replace_gui.py
+++ find_replace_gui.py
@@ -4,37 +4,46 @@ import streamlit as st
 # Define the regular expressions to search for and their replacements
 patterns = [
     (r'<p.*?>', '<p style="margin: 20px 0;line-height: 1.5;">'),
-    (r'<li.*?>', '<li><p style="margin: 20px 0;line-height: 1.5;">'),
-    (r'</li>', '</p></li>'),
+    #(r'<li.*?>', '<li><p style="margin: 20px 0;line-height: 1.5;">'),
+    #(r'</li>', '</p></li>'),
     (r'</?span.*?>', ''),
     (r'<h1.*?>', '<h1 style="margin: 20px 0;line-height: 1.5;font-size: 30px;font-weight: 400;">'),
     (r'<h2.*?>', '<h2 style="margin: 20px 0;line-height: 1.5;font-size: 24px;font-weight: 400;">'),
     (r'<h3.*?>', '<h3 style="margin: 20px 0;line-height: 1.5;font-size: 20px;font-weight: 400;">'),
     (r'<h4.*?>', '<h4 style="margin: 20px 0;line-height: 1.5;font-size: 18px;font-weight: 700;">'),
     (r'<table.*?>', '<table border="1" class="table-bordered" style=”border-collapse:collapse;”>'),
-    (r'<img ', '<img class="img-responsive fr-fic fr-dii" ')
+    (r'<img ', '<img class="img-responsive fr-fic fr-dii" '),
+    (r'<p style="margin: 20px 0;line-height: 1.5;">&nbsp;</p>', ''),
+    (r'<p style="margin: 20px 0;line-height: 1.5;"><p style="margin: 20px 0;line-height: 1.5;">', '<p style="margin: 20px 0;line-height: 1.5;">'),
+    (r'</p></p>', '</p>')
 ]
 
 # Define the function to apply the substitutions
-def apply_substitutions(text):
+def apply_substitutions():
+    # Get the text from the input box
+    text = input_box.text_area('Input HTML', height=200)
+
+    # Apply the <strong> substitution for <span> tags
+    text = re.sub(r'<span style="font-weight:700">(.*?)</span>', r'<strong>\1</strong>', text)
+
+    # Apply the <i> substitution for <span> tags
+    text = re.sub(r'<span style="font-style:italic">(.*?)</span>', r'<i>\1</i>', text)
+
+    # Add paragraph tag to list items
+    text = re.sub(r'<li.*?>(.*?)</li>', r'<li><p style="margin: 20px 0;line-height: 1.5;">\1</p></li>', text)
+
     # Apply the regular expressions and replacements to the text
     for pattern, replacement in patterns:
         text = re.sub(pattern, replacement, text)
 
-    return text
+    # Display the modified HTML
+    st.text_area('Modified HTML', value=text, height=200)
 
 # Set up the Streamlit app
-def main():
-    st.title('HTML Substitution Tool')
+st.title('HTML Substitution Tool')
 
-    # Create the input box for the user to paste in HTML
-    input_text = st.text_area('Input HTML', height=200)
+# Create the button to apply the substitutions
+st.button('Apply Substitutions', on_click=apply_substitutions)
 
-    # Create the button to apply the substitutions
-    if st.button('Apply Substitutions'):
-        output_text = apply_substitutions(input_text)
-        # Display the modified HTML
-        st.text_area('Modified HTML', value=output_text, height=200)
-
-if __name__ == '__main__':
-    main()
+# Create the input box for the user to paste in HTML
+input_box = st.empty()
